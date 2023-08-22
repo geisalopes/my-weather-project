0a60c6ce9cf10febbbbcffb7936cc5d36e6a471a
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-08-22 14:52:46 +0200

    made some changes in html and js

--- src/index.js
+++ src/index.js
@@ -27,26 +27,16 @@ let currentTime = new Date();
 
 actualDate.innerHTML = formatDate(currentTime);
 
-let enterNameCity = document.querySelector("form");
-enterNameCity.addEventListener("submit", searchCity);
-
-function searchCity(event) {
-  event.preventDefault();
-  let cityField = document.querySelector("input");
-  let city = cityField.value;
-  let apiKey = "9d85a623d5f54fa249d3910c26ca0525";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showCityTemperature);
-}
+// Homework 5
 
 function showCityTemperature(response) {
-  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  console.log(response.data.weather[0].main);
 
   document.querySelector("#windSpeed").innerHTML = Math.round(
     response.data.wind.speed
@@ -54,14 +44,34 @@ function showCityTemperature(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 }
 
-let currentLocation = document.querySelector("#current");
+function searchCity(city) {
+  let apiKey = "9d85a623d5f54fa249d3910c26ca0525";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showCityTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityField = document.querySelector("input");
+  let city = cityField.value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "9d85a623d5f54fa249d3910c26ca0525";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showCityTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let enterNameCity = document.querySelector("#search-form");
+enterNameCity.addEventListener("submit", handleSubmit);
+
+let currentLocation = document.querySelector("#current-location-button");
 currentLocation.addEventListener("click", getCurrentLocation);
 
-function getCurrentLocation() {
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-function showPosition(position) {
-  console.log(position);
-  let h1 = document.querySelector("h1");
-}
+searchCity("New York");
